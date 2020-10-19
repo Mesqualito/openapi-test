415c038cc7e9e3eb3836987a772acdd560ff0f03
Mesqualito <jochen@gebsattel.rocks>
2020-10-19 11:35:58 +0200

    Improve OpenAPI UI Metadata (1) - Konfiguration
    see: https://www.udemy.com/course/spring-framework-5-beginner-to-guru/learn/lecture/8878452 - Lektion 25-399

--- src/main/java/com/eigenbaumarkt/spring5mvc/restapplication/configuration/SwaggerConfig.java
+++ src/main/java/com/eigenbaumarkt/spring5mvc/restapplication/configuration/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 // enables OpenApi in project
 @EnableSwagger2
@@ -23,6 +27,25 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     }
+
+    // improve Swagger default Metadata
+    private ApiInfo metaData() {
+
+        // springfox.documentation.service.Contact
+        Contact contact = new Contact("Jochen Gebsattel", "https://www.gebsattel.rocks", "jochen@gebsattel.rocks");
+
+                return new ApiInfo(
+                        "JHipster Guru",
+                        "but first - Spring framework 5: Beginner to Guru",
+                        "1.0",
+                        "Terms of service: just learning, just kidding!",
+                        contact,
+                        "Apache License Version 2.0",
+                        "https://www.apache.org/license/LICENSE-2.0",
+                        new ArrayList<>());
+    }
+
 }
