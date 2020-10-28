308c916f728882c6f0a8533ee20896e78c7fa4f3
Mesqualito <jochen@gebsattel.rocks>
2020-10-28 14:41:00 +0100

    Original sources, but unable to run nor to 'package' spring5-mvc-model. BUG !
    see: https://www.udemy.com/course/spring-framework-5-beginner-to-guru/learn/lecture/9164302 - Lektion 27-432

--- rest-application/src/main/java/com/eigenbaumarkt/spring5mvc/restapplication/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eigenbaumarkt.spring5mvc.restapplication.repositories;
-
-import com.eigenbaumarkt.spring5mvc.restapplication.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByName(String name);
-}
--- rest-application/src/main/java/com/eigenbaumarkt/spring5mvc/restapplication/repositories/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eigenbaumarkt.spring5mvc.restapplication.repositories;
-
-import com.eigenbaumarkt.spring5mvc.restapplication.domain.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    // Customer findByFirstnameAndLastname(String firstname, String lastname);
-}
